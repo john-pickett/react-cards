60df6a5ec2439de0ca6845508e67175e06a9922d
John P <john.pickett@gmail.com>
2018-02-19 06:13:19 -0600

    adding inputs for create

--- src/components/CreateLesson.js
+++ src/components/CreateLesson.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import '../App.css';
 import Navbar from './Navbar';
 
-class CreateLesson extends Component {
+import axios from 'axios';
 
+class CreateLesson extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            length: 1,
+            timer: 30
+        }
+    }
+
+    saveLesson = () => {
+        console.log('lesson saved');
+    }
     render () {
         return (
             <div>
@@ -11,23 +24,28 @@ class CreateLesson extends Component {
                 <h2>Create New Lesson</h2>
                 <div className="row">
                     <div className="col-md-6">
-                        <div className="card">
-                        <span className="card-title">Words To Guess</span>
-                            <input type="text" />
-                            <label>Lesson Title</label>
-                            <input type="text" />
-                            <label>Input</label>
+                        <div className="card create-lesson-card">
+                        <span className="card-title">Flash Cards</span>
+                            <div className="md-form">
+                                <input type="text" id="lesson-title" className="form-control" />
+                                <label htmlFor="lesson-title">Lesson Title</label>
+                                <input type="text" id="card1" className="form-control" />
+                                <label htmlFor="card1">Input</label>
+                                <input type="text" id="card2" className="form-control" />
+                                <label htmlFor="card2" >Input</label>
+                            </div>
+                            
                             <p>The word and its answer must be on the same row.</p>
                         </div>
                     </div>
                     <div className="col-md-6">
-                        <div className="card">
+                        <div className="card create-lesson-card">
                         <span className="card-title">Correct Answers</span>
                             <input type="text" />
                             <label>Input</label>
                             <input type="text" />
                             <label>Input</label>
-                            <button className="btn btn-primary">Save</button>
+                            <button className="btn btn-primary" onClick={this.saveLesson}>Save</button>
                         </div>
                     </div>
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,35 +7,35 @@ class Navbar extends Component {
     render () {
         return (
                 <div>
-                    <nav class="navbar navbar-expand-lg navbar-dark indigo">
-                        {/* <a class="navbar-brand" href="#">Flash Cards</a> */}
+                    <nav className="navbar navbar-expand-lg navbar-dark indigo">
+                        {/* <a className="navbar-brand" href="#">Flash Cards</a> */}
                         <Link to='/'>Home</Link>
                         <Link to='/create-lesson'>Create Lesson</Link>
                         <Link to='/profile'>Your Profile</Link>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                        {/* <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto">
-                                 <li class="nav-item active">
-                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                 <li className="nav-item active">
+                                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                                 </li> 
-                                <li class="nav-item">
+                                <li className="nav-item">
                                     <Link to='/create-lesson'>Create Lesson</Link>
                                 </li>
-                                <li class="nav-item">
+                                <li className="nav-item">
                                     <Link to='/profile'>Your Profile</Link>
                                 </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                                    <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <a class="dropdown-item" href="#">Something else here</a>
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                                    <div className="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                                        <a className="dropdown-item" href="#">Action</a>
+                                        <a className="dropdown-item" href="#">Another action</a>
+                                        <a className="dropdown-item" href="#">Something else here</a>
                                     </div>
                                 </li>
                             </ul>
-                            <form class="form-inline">
-                                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                            <form className="form-inline">
+                                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
                             </form>
                         </div> */}
                     </nav>
